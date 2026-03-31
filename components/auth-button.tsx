ce195afd81b1ce6a8baad1ac891f6d397c37ed8a
rheactdev <rheamanuel02@gmail.com>
2026-03-31 01:51:59 -0400

    feat: refactor auth-button to use details element for user session management

--- components/auth-button.tsx
+++ components/auth-button.tsx
@@ -8,26 +8,18 @@ export async function AuthButton() {
   const { data } = await supabase.auth.getClaims();
 
   const user = data?.claims;
+  if (!user) {
+    return null
+  }
 
-  return user ? (
-    <div className="dropdown dropdown-end">
-      <div tabIndex={0} role="button" className="btn btn-ghost btn-sm">
-        {user.email}
-      </div>
-      <ul tabIndex={0} className="dropdown-content menu bg-base-200 rounded-box z-50 w-52 p-2 shadow-lg mt-2">
-        <li>
-          <LogoutButton />
-        </li>
-      </ul>
-    </div>
-  ) : (
-    <div className="flex gap-2">
-      <Link href="/auth/login" className="btn btn-outline btn-sm">
-        Sign in
-      </Link>
-      <Link href="/auth/sign-up" className="btn btn-primary btn-sm">
-        Sign up
-      </Link>
-    </div>
-  );
+  return (
+    <li>
+      <details className="">
+        <summary tabIndex={0}>{user.email}</summary>
+        <ul tabIndex={-1} className="bg-base-300 rounded-t-none p-2 right-0">
+          <li><LogoutButton /></li>
+        </ul>
+      </details>
+    </li>
+  )
 }
